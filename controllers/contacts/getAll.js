9979b7ba422c90498da4c83bb139c79615899e55
IhorPshenychnyi <75953411+IhorPshenychnyi@users.noreply.github.com>
2021-11-22 23:14:20 +0200

    fix:

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -13,10 +13,10 @@ const getAll = async (req, res, next) => {
     //           Фильтрация контактов по полю избранного (GET /contacts?favorite=true)
 
     if (favorite === 'true') {
-      const contacts = await Contact.find(
-        { owner: _id },
-        '_id name email phone favorite',
-      ).populate('owner', '_id email')
+      const contacts = await Contact.find({ owner: _id }).populate(
+        'owner',
+        '_id email',
+      )
 
       contacts.forEach(contact => {
         if (contact.favorite === true) {
@@ -35,22 +35,40 @@ const getAll = async (req, res, next) => {
     }
     //                                   Пагинация колекции контактов
 
-    const skip = (page - 1) * limit
-    const contacts = await Contact.find(
-      { owner: _id },
-      '_id name email phone favorite',
-      {
-        skip,
-        limit,
-      },
-    ).populate('owner', '_id email')
+    if (page && limit) {
+      const skip = (page - 1) * limit
+      const contacts = await Contact.find(
+        { owner: _id },
+        '_id name email phone favorite',
+        {
+          skip,
+          limit,
+        },
+      ).populate('owner', '_id email')
+
+      return res.json({
+        status: 'success',
+        code: 200,
+        message: 'Contacts found',
+        data: {
+          contacts,
+        },
+      })
+    }
+
+    //                          Поиск всех контактов пользователя без применения пагинации
+
+    const result = await await Contact.find({ owner: _id }).populate(
+      'owner',
+      '_id email',
+    )
 
     return res.json({
       status: 'success',
       code: 200,
       message: 'Contacts found',
       data: {
-        contacts,
+        result,
       },
     })
   } catch (error) {
